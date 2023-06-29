f0f4f33cb50b96bdf1d2ce1e148a1c84b6c0cdc2
irakli-tkemaladze <136730129+irakli-tkemaladze@users.noreply.github.com>
2023-06-30 00:46:23 +0400

    Add files via upload

--- to do tkemaladze/main.js
+++ to do tkemaladze/main.js
@@ -1,19 +1,15 @@
 let taskName = document.getElementById("title");
 let description = document.getElementById("description");
 let assignee = document.getElementById("assignee");
-let state = document.getElementById("state");
-let toDoArea = document.getElementById("toDoArea");
-let progressArea = document.getElementById("progressArea");
-let testingArea = document.getElementById("testingArea");
-let doneArea = document.getElementById("doneArea");
 
-function createElement(optionValue) {
+function createElement() {
+    let state = document.getElementById("state").value;
     const forText = document.createElement("article");
     const startTextSection = document.createElement("aside");
     const taskTitle = document.createElement("h1");
     const taskDescription = document.createElement("h3");
     const endText = document.createElement("p");
-    optionValue.appendChild(forText);
+    document.getElementById(state).appendChild(forText);
     forText.appendChild(startTextSection);
     startTextSection.appendChild(taskTitle);
     startTextSection.appendChild(taskDescription);
@@ -31,22 +27,9 @@ function reset() {
 
 
 function doit() {
-    if (state.value === "To-do" && taskName.value != "" && description.value != "" && assignee.value != "") {
-        createElement(toDoArea);
+    if (taskName.value != "" && description.value != "" && assignee.value != "") {
+        createElement();
         reset();
-
-    } else if (state.value === "progress" && taskName.value != "" && description.value != "" && assignee.value != "") {
-        createElement(progressArea);
-        reset();
-
-    } else if (state.value === "testing" && taskName.value != "" && description.value != "" && assignee.value != "") {
-        createElement(testingArea);
-        reset();
-
-    } else if (state.value === "done" && taskName.value != "" && description.value != "" && assignee.value != "") {
-        createElement(doneArea);
-        reset();
-
     } else {
         window.alert('დაშვებულია შეცდომა, გთხოვთ შეავსოთ სრული ფორმა')
     }
